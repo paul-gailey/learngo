8a9c6c755520a95dcd7b1e4d2834990b769bc129
Paul Gailey <paul.gailey@icloud.com>
2024-07-07 21:00:14 +0100

    solution using fmt.

--- 02-write-your-first-program/exercises/01-print-names/main.go
+++ 02-write-your-first-program/exercises/01-print-names/main.go
@@ -8,6 +8,8 @@
 
 package main
 
+import "fmt"
+
 // ---------------------------------------------------------
 // EXERCISE: Print names
 //
@@ -24,6 +26,6 @@ package main
 // ---------------------------------------------------------
 
 func main() {
-	println("PaulGailey")
-	println("JasonKurth")
+	fmt.Println("PaulGailey")
+	fmt.Println("JasonKurth")
 }
