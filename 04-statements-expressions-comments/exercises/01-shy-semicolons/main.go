6e6dac2a5ce73d4ff31f06c4319f7147731182c1
Paul Gailey <paul.gailey@icloud.com>
2024-07-08 16:43:18 +0100

    04-statements-expressions-comments exercise 01-shy-semicolons solutions

--- 04-statements-expressions-comments/exercises/01-shy-semicolons/main.go
+++ 04-statements-expressions-comments/exercises/01-shy-semicolons/main.go
@@ -8,6 +8,8 @@
 
 package main
 
+import "fmt"
+
 // ---------------------------------------------------------
 // EXERCISE: Shy Semicolons
 //
@@ -19,4 +21,7 @@ package main
 // ---------------------------------------------------------
 
 func main() {
+	fmt.Println("Hello")
+	fmt.Println("World")
+	fmt.Println("My")
 }
